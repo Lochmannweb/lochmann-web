a6833255ece0c9696394cd01bbb2d7f93aab9571
Lochmannweb <lochmannweb@gmail.com>
2025-01-04 17:50:49 +0100

    meta description

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,13 +4,14 @@ import CustomerInformation from "./components/CustomerInformation";
 
 export const metadata = {
   title: "LochmannWeb",
-  description: "",
+  description: "Enhance Your Website's Performance & Impact",
 }; 
 
 export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <link rel="icon" type="image/png" sizes="32x32" href="/logo.png" />
       <body>
         <Menu />
           <main>
